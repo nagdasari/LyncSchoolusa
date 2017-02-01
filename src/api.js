bc7cd48ee830859fc70a77d3e67cc36388e9b319
SanjeeviniRao <sanjeevini@digitallyncs-MacBook-Air.local>
2017-02-01 18:35:13 +0530

    removed passport code

--- src/api.js
+++ src/api.js
@@ -16,34 +16,13 @@ this.init();
 
 init(){
 
-passport.use(new LocalStrategy(function(username,password,done){
-User.getUserByUsername(username,function(err,user){
-    if(err) throw err;
-    if(!user){
-        return done(null,false,{message:'Unknown user'});
-    }
-    User.comparePassword(password,user.password,function(err,isMatch){
-        if(err) throw err;
-        if(isMatch){
-            return done(null,user);
-            
-        }else{
-            return done(null,false,{message:'Invalid password'});
-        }
-    });
-});
-    
-}));
-    
-    
+
     
     
 this.router.post('/reg', reg.registerUser.bind(express)); 
 this.router.post('/footerContact',foo.FooterContact1.bind(express) );
     
-this.router.post('/login',passport.authenticate('local',{successRedirect:'/',failureRedirect:'/login'}), function(request,response){
-    
-});    
+
 
 }
 
@@ -52,13 +31,5 @@ this.router.post('/login',passport.authenticate('local',{successRedirect:'/',fai
 }
 
 
-module.exports.getUserByUsername =  function(username,callback){
-    var query = {email:username};
-    User.findOne(query,callback);
-}
-
-module.exports.comparePassword = function(candidatepassword,callback){
-    console.log(hi);
-}
 
 module.exports = Api;
