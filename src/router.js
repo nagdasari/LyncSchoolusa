a43aa1394a2c3cbfba4b16a232e28572a562cec9
SanjeeviniRao <sanjeevini@digitallyncs-MacBook-Air.local>
2017-02-06 17:54:06 +0530

    added courses page dynamically

--- src/router.js
+++ src/router.js
@@ -6,6 +6,9 @@ let passport = require('passport');
 let verify = require('./controllers/verify_user');
 let fp = require('./controllers/forgotpassword');
 let rp = require('./controllers/resetpassword');
+let courses = require('./controllers/courses');
+let subcourses = require('./controllers/subcourses');
+let subcoursemain = require('./controllers/subcoursemain');
 
 class Router {
 
@@ -35,7 +38,16 @@ this.router.get('/register',(request,response) => {
 this.router.get('/profile',(request,response) => {
    response.render('profile'); 
 });   
-	  
+
+this.router.get('/program',(request,response) => {
+   response.render('programs'); 
+});       
+  
+this.router.get('/about',(request,response) => {
+   response.render('about'); 
+});       
+          
+      
 this.router.get('/thankyou',(request,response) => {
    response.render('thankyou'); 
 });
@@ -81,6 +93,59 @@ this.router.get('/auth/google/callback',
                     successRedirect : '/profile',
                     failureRedirect : '/login'
             }));
+      
+ /* added routes for courses  dynamic */     
+this.router.get('/courses', (req, res) => {
+            courses.getCoursesList().then(function(ele) {
+                // for(var t =0; t<ele.length; t++){
+                //console.log(" In curses route" +  ele[t].id  + " " + ele[t].coursename + "" );
+                //}
+                res.render('courseslist', { coursedata: ele });
+            }).catch(function(err) {
+                console.log("In courses route" + err);
+
+            });
+        });
+
+      
+this.router.get('/:name', (req, res) => {
+            var raw_url = req.url.toString();
+         var  new_url =  raw_url.replace('/','');
+           subcourses.getCoursesList(new_url).then(function(ele1) {
+                for (var t = 0; t < ele1.length; t++) {
+                    console.log(" In curses1 route" + ele1[t].subcourseid + " " + ele1[t].subcoursename + "");
+                }
+
+                res.render('subcourses', { subcoursedata: ele1 });
+
+
+            }).catch(function(err) {
+                console.log("In courses route" + err);
+
+            });
+           // res.render('balaji');
+        });
+
+
+        this.router.get('/training/:name1', (req,res)=>{
+
+            var raw_url = req.url.toString().split("/");
+            var new_url = raw_url[2];
+            console.log("here" +  new_url);
+      //  console.log("sanju" +  req.url);
+            subcoursemain.getModuleContent(new_url).then(function(ele2) {
+              console.log(" In curses2 route" + ele2[0].title + " " + ele2[0].description + "" + ele2[0].modules.length);
+               res.render('coursesmain', { maincoursedata: ele2 });
+          });
+
+                        });      
+      
+      
+     /* added routes for courses  dynamic */ 
+      
+      
+      
+      
 
  
   }
